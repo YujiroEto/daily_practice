a7d33df578947363c6a162d10e1735e57649de72
yujiro eto <yujro.eto@gmail.com>
2013-06-26 19:53:38 +0900

    6/26_dumper

---
+++ Damper/test.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my $var = {
+	array => [1,2,3],
+	hash => {
+		key1 => "val1", 
+		key2 => "val2"
+	},
+	coderef => sub { "this is coderef"},
+	test => [3,5,6,7,8]
+};
+
+print Dumper $var;
+
