ccc621c757bf03dead63c140b1eeb09b2cadc07c
Yujiro Eto <yujro.eto@gmail.com>
2013-08-18 20:45:49 +0900

    validator

--- FormValidator_Simple/test.pl
+++ FormValidator_Simple/test.pl
@@ -1,20 +1,23 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-
+use YAML;
 use Data::Dumper;
 use FormValidator::Simple;
 
+FormValidator::Simple->set_messages('/Users/Yujiro/git_YujiroEto/daily_practice/FormValidator_Simple/message.yml');
+#FormValidator::Simple->set_mesasges_decode_from('utf-8');
+
 my $params = +{
     param1 => '12345',
-	#param2 => 'abcdefg',
-    param2 => '123',
+	param2 => 'abcdefg',
+	#param2 => '123',
 	#param3 => undef,
     param3 => 'a',
     mail1  => 'test@example.com',
     mail2  => 'test2@example.com',
     year   => 2010,
-    month  => 2,
+    month  => 02,
     day    => 30,
 };
 
@@ -30,14 +33,18 @@ my $result = FormValidator::Simple->check(
     ]
 );
 
-print $result->has_error."\n";
+#my $message = $result->messages('action1');
+
+my $message = $result->field_messages('action1');
+print Dumper $message;
+#print $result->has_error."\n";
 #print Dumper $result->error;
 #print Dumper $params;
 #print $result->error."\n";
-
-if ( $result->has_error ) {
-    for my $key ( $result->error ) {
-        my @types = map { $_ } $result->error($key);
-        printf "$key:%s\n", join q{,}, @types;
-    }
-}
+#
+#if ( $result->has_error ) {
+#    for my $key ( $result->error ) {
+#        my @types = map { $_ } $result->error($key);
+#        printf "$key:%s\n", join q{,}, @types;
+#    }
+#}
---
+++ RegularExpression/test.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+/^(\xA1\xBC|\xA4[\xA1-\xF3])+$/
+
